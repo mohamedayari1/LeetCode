a2d55fe72d1ccf8a1cce02c8c402bde1f29cdab6
mohamedayari1 <ayarim781@gmail.com>
2024-07-27 06:40:22 +0100

    Time: 138 ms (40.19%), Space: 31.1 MB (67.53%) - LeetHub

--- 1448-count-good-nodes-in-binary-tree/1448-count-good-nodes-in-binary-tree.py
+++ 1448-count-good-nodes-in-binary-tree/1448-count-good-nodes-in-binary-tree.py
@@ -6,16 +6,20 @@
 #         self.right = right
 class Solution:
     def goodNodes(self, root: TreeNode) -> int:
-        def dfs(node, max_value):
+        self.result = 0
+        
+        
+        def preorder_dfs(node, max_value):
             if not node:
                 return 0
             
-            result = 1 if node.val >= max_value else 0
+            #Implementing preorder dfs
+            self.result = 1 if node.val >= max_value else 0
             max_value = max(max_value, node.val)
             
-            result += dfs(node.left, max_value)
-            result += dfs(node.right, max_value)
+            self.result += preorder_dfs(node.left, max_value) + preorder_dfs(node.right, max_value)
+            
+            return self.result 
 
-            return result 
-        result = dfs(root, root.val)
-        return result 
+        self.result = preorder_dfs(root, root.val)
+        return self.result
