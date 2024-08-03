e5519b2af023f38e8a88c3896d9cde066c334f9c
mohamedayari1 <ayarim781@gmail.com>
2024-08-03 07:42:12 +0100

    Time: 138 ms (30.47%), Space: 89 MB (5.56%) - LeetHub

--- 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.py
+++ 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.py
@@ -1,38 +1,37 @@
+# Definition for a binary tree node.
 # class TreeNode:
 #     def __init__(self, val=0, left=None, right=None):
 #         self.val = val
 #         self.left = left
 #         self.right = right
-
 class Solution:
-    def _find_preorder_and_inorder(self, preorder: List[int], inorder: List[int]) -> Tuple[Tuple[List[int], List[int]], Tuple[List[int], List[int]]]:
-        """ Finds the inorder and preorder lists of the left and right subtrees. """
-        root_val = preorder[0]
 
-        i = inorder.index(root_val)
+    def helper(self, preorder: List[int], inorder: List[int]) -> Tuple[Tuple[List[int], List[int]], Tuple[List[int], List[int]]]:
+        
+        root = preorder[0]
+        index = inorder.index(root)
 
-        left_inorder = inorder[:i]
-        right_inorder = inorder[i+1:]
+        left_inorder, right_inorder = inorder[:index], inorder[index + 1:]
+        left_preorder, right_preorder = preorder[1:len(left_inorder) + 1], preorder[len(left_inorder)+1:]
 
-        left_preorder = preorder[1:len(left_inorder)+1]
-        right_preorder = preorder[len(left_inorder)+1:]
+        left_lists  = (left_preorder, left_inorder)
+        right_lists  = (right_preorder, right_inorder)
 
-        left_lists = (left_preorder, left_inorder)
-        right_lists = (right_preorder, right_inorder)
         return left_lists, right_lists
 
     def buildTree(self, preorder: List[int], inorder: List[int]) -> Optional[TreeNode]:
         if not preorder or not inorder:
             return None
-        
-        root_val = preorder[0]
-        root_node = TreeNode(root_val)
 
-        left_lists, right_lists = self._find_preorder_and_inorder(preorder, inorder)
-        left_preorder, left_inorder = left_lists
-        right_preorder, right_inorder = right_lists
 
-        root_node.left = self.buildTree(left_preorder, left_inorder)
-        root_node.right = self.buildTree(right_preorder, right_inorder)
+        root_value = preorder[0]
+        root_node  = TreeNode(root_value)
+
+        left_lists, right_lists = self.helper(preorder, inorder)
+
+        root_node.left = self.buildTree(left_lists[0], left_lists[1])
+        root_node.right = self.buildTree(right_lists[0], right_lists[1])
 
         return root_node
+
+        
