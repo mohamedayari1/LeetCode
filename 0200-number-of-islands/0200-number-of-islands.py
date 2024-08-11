25b11f806fed07c72bfa382e44f227d76f014dd8
mohamedayari1 <ayarim781@gmail.com>
2024-08-11 09:02:02 +0100

    Time: 314 ms (12.65%), Space: 24.4 MB (21.70%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -1,33 +1,31 @@
 class Solution:
-
     def numIslands(self, grid: List[List[str]]) -> int:
-        raws, cols = len(grid), len(grid[0])
-        visited = set()        
-        nbr_islands = 0
+        visited = set()
+        ROWS, COLS = len(grid), len(grid[0])
 
-        def bfs(r, c):
+        def helper(r, c):
             directions = [[1, 0], [-1, 0], [0, 1], [0, -1]]
             queue = collections.deque([(r, c)])
+            visited.add((r, c))
+
             while queue:
                 for _ in range(len(queue)):
-                    raw, col = queue.popleft()
+                    row, col = queue.popleft()
                     for dr, dc in directions:
-                        r, c = raw + dr, col + dc
+                        nr, nc = row + dr, col + dc
 
-                        if (r in range(raws) and
-                            c in range(cols) and 
-                            (r, c) not in visited and
-                            grid[r][c] == "1"  
-                        ):
-                            queue.append((r, c))
-                            visited.add((r, c))
+                        if (nr in range(ROWS) and 
+                            nc in range(COLS) and 
+                            (nr, nc) not in visited and 
+                            grid[nr][nc] == "1"):
 
+                            visited.add((nr, nc))
+                            queue.append((nr, nc))
 
-        for r in range(raws):
-            for c in range(cols):
-                if (grid[r][c] == "1" and 
-                    (r, c) not in visited):
-                    bfs(r, c)
-                    nbr_islands += 1
-        return nbr_islands    
-                
+        result = 0
+        for r in range(ROWS):
+            for c in range(COLS):
+                if grid[r][c] == "1" and (r, c) not in visited:
+                    helper(r, c)
+                    result += 1
+        return result
