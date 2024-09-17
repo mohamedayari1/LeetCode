8b22ddc5ea5c6d5da7d8c760f911a0ea01819d98
mohamedayari1 <ayarim781@gmail.com>
2024-09-17 08:38:46 +0100

    Time: 303 ms (16.98%), Space: 24 MB (38.10%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -1,30 +1,30 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
-        visited = set()
-        ROWS, COLS  = len(grid), len(grid[0])
+        ROWS, COLS = len(grid), len(grid[0])
+        seen = set()
+        queue = collections.deque()
 
         def bfs(r, c):
-            queue = collections.deque([(r, c)])
-            visited.add((r, c))
-            directions = [[1, 0],[-1, 0],[0, 1],[0, -1]]
+            queue.append((r, c))
+            seen.add((r, c))
+            directions =[[1, 0], [-1, 0], [0, 1], [0, -1]]
             while queue:
                 for _ in range(len(queue)):
                     row, col = queue.popleft()
                     for dr, dc in directions:
-                        nr, nc = row + dr, col + dc 
-
-                        if (nr in range(ROWS) and
-                            nc in range(COLS) and
-                            grid[nr][nc] == "1" and
-                            (nr, nc) not in visited):
+                        nr, nc = row + dr, col + dc
+                        if (nr in range(ROWS) and 
+                            nc in range(COLS) and 
+                            grid[nr][nc] == '1' and 
+                            (nr, nc) not in seen):
 
                             queue.append((nr, nc))
-                            visited.add((nr, nc))
-        
-        num_of_islands = 0
+                            seen.add((nr, nc))
+
+        result = 0
         for r in range(ROWS):
             for c in range(COLS):
-                if grid[r][c] == "1" and (r, c) not in visited:
+                if grid[r][c] == '1' and (r, c) not in seen:
                     bfs(r, c)
-                    num_of_islands += 1
-        return num_of_islands
+                    result += 1
+        return result 
