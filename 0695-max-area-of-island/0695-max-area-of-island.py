b4295b5b136945c9c5aba2cb88fe87579166aee3
mohamedayari1 <ayarim781@gmail.com>
2024-09-17 09:30:36 +0100

    Time: 128 ms (13.58%), Space: 17 MB (80.96%) - LeetHub

--- 0695-max-area-of-island/0695-max-area-of-island.py
+++ 0695-max-area-of-island/0695-max-area-of-island.py
@@ -1,37 +1,33 @@
-from typing import List
-import collections
-
 class Solution:
     def maxAreaOfIsland(self, grid: List[List[int]]) -> int:
+        ROWS, COLS = len(grid), len(grid[0])
         visited = set()
-        raws, cols = len(grid), len(grid[0])
-
-        max_area = 0 
+        queue = deque()
 
         def bfs(r, c):
-            queue = collections.deque([(r, c)])
+            queue.append((r, c))
+            visited.add((r, c))
             directions = [[1, 0], [-1, 0], [0, 1], [0, -1]]
-            current_area = 1  
-            
+            area = 1
             while queue:
-                raw, col = queue.popleft()
-                for dr, dc in directions:
-                    nr, nc = raw + dr, col + dc
-                    if (
-                        nr in range(raws) and 
-                        nc in range(cols) and 
-                        grid[nr][nc] == 1 and 
-                        (nr, nc) not in visited):
-                        
-                        queue.append((nr, nc))
-                        visited.add((nr, nc))
-                        current_area += 1
-            return current_area
-                    
-
-        for r in range(raws):
-            for c in range(cols):
-                if (r, c) not in visited and grid[r][c] == 1:
-                    visited.add((r, c))  
-                    max_area = max(max_area, bfs(r, c))
-        return max_area
+                for _ in range(len(queue)):
+                    row, col = queue.popleft()
+                    for dr, dc in directions:
+                        nr, nc = row + dr, col + dc
+                        if (nr in range(ROWS) and 
+                            nc in range(COLS) and
+                            grid[nr][nc] == 1 and 
+                            (nr, nc) not in visited):
+                            
+                            area += 1
+                            queue.append((nr, nc))
+                            visited.add((nr, nc))
+            
+            return area 
+        max_area = 0
+        
+        for r in range(ROWS):
+            for c in range(COLS):
+                if grid[r][c] == 1 and (r, c) not in visited:
+                    max_area = max(bfs(r, c), max_area)
+        return max_area 
