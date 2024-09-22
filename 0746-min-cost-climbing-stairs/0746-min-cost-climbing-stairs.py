ef2fa1beafc121752992f29961f34bc92b5d0936
mohamedayari1 <ayarim781@gmail.com>
2024-09-22 10:29:59 +0100

    Time: 48 ms (89.78%), Space: 16.7 MB (66.45%) - LeetHub

--- 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.py
+++ 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.py
@@ -1,17 +1,18 @@
 class Solution:
     def minCostClimbingStairs(self, cost: List[int]) -> int:
-
+        cost.append(0)
         n = len(cost)
-        self.cache = {n-1: cost[n-1], n-2: cost[n-2]}
-        prices = []
-
-        def helper(i, cost):
-            if i in self.cache:
-                return self.cache[i]
-
+        # cache = {n-1: cost[n-1], n-2: cost[n-2]}
+        # def helper(i, cache):
+        #     if i in cache:
+        #         return cache[i]
+        #     cache[i] = cost[i] + min(helper(i+1, cache), helper(i+2, cache))
             
-            self.cache[i] = cost[i] + min(helper(i+1, cost), helper(i+2, cost))
-            
-            return self.cache[i]
+        #     return cache[i] 
 
-        return min(helper(0, cost), helper(1, cost))
+
+        # return min(helper(0, cache), helper(1, cache))
+
+        for i in range(n-3, -1, -1):
+            cost[i] += min(cost[i+1], cost[i+2])
+        return min(cost[0], cost[1])
