cf7ffa53f602718d144ad950ebddf7d658c2e823
mohamedayari1 <ayarim781@gmail.com>
2024-10-28 12:43:51 +0100

    Time: 14 ms (51.74%), Space: 63.1 MB (50.50%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.ts
+++ 0217-contains-duplicate/0217-contains-duplicate.ts
@@ -1,12 +1,15 @@
 function containsDuplicate(nums: number[]): boolean {
-    const seen: Set<number> = new Set();
-    for (const num of nums) {
+    const seen: Set<number> = new Set(); 
+
+    for (let i=0; i< nums.length; i++) {
+        const num = nums[i];
         if (seen.has(num)) {
             return true;
         }
+
         seen.add(num);
     }
     return false;
 
 
-};
+};  
